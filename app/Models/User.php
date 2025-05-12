4416208d3fc0ab8d454f305505eceece6f894f14
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-05-13 00:51:50 +0300

    as

--- app/Models/User.php
+++ app/Models/User.php
@@ -200,25 +200,4 @@ class User extends Administrator implements JWTSubject
     }
 
     //setter for languages_spoken if is array to json
-    public function setLanguagesSpokenAttribute($value)
-    {
-        if (is_array($value)) {
-            $this->attributes['languages_spoken'] = json_encode($value);
-        } else {
-            $this->attributes['languages_spoken'] = $value;
-        }
-    } 
-
-    //getter for languages_spoken if is json to array
-    public function getLanguagesSpokenAttribute($value)
-    {
-        if ($value == null || strlen($value) < 3) {
-            return [];
-        }
-        $value = json_decode($value, true);
-        if ($value == null) {
-            return [];
-        }
-        return $value;
-    } 
 }
