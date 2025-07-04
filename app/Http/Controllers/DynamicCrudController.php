73a97206b09bfbef4f3b574881db1973a4b489a3
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-07-05 01:38:14 +0300

    Restore filtering logic for company and user in index method of DynamicCrudController

--- app/Http/Controllers/DynamicCrudController.php
+++ app/Http/Controllers/DynamicCrudController.php
@@ -203,19 +203,19 @@ class DynamicCrudController extends Controller
         $validColumns = Schema::getColumnListing($table);
         $query = $modelClass::query();
 
-        // $isNotForCompany = $request->query('is_not_for_company');
-        // if ($isNotForCompany !== 'yes' && !$u->isRole('super-admin') && in_array('enterprise_id', $validColumns)) {
-        //     $query->where('enterprise_id', $u->enterprise_id);
-        // }
+        $isNotForCompany = $request->query('is_not_for_company');
+        if ($isNotForCompany !== 'yes' && !$u->isRole('super-admin') && in_array('enterprise_id', $validColumns)) {
+            $query->where('enterprise_id', $u->enterprise_id);
+        }
 
-        // $isNotForUser = $request->query('is_not_for_user');
-        // if ($isNotForUser !== 'yes' && !$u->isRole('super-admin')) {
-        //     if (in_array('administrator_id', $validColumns)) {
-        //         $query->where('administrator_id', $u->id);
-        //     } elseif (in_array('user_id', $validColumns)) {
-        //         $query->where('user_id', $u->id);
-        //     }
-        // }
+        $isNotForUser = $request->query('is_not_for_user');
+        if ($isNotForUser !== 'yes' && !$u->isRole('super-admin')) {
+            if (in_array('administrator_id', $validColumns)) {
+                $query->where('administrator_id', $u->id);
+            } elseif (in_array('user_id', $validColumns)) {
+                $query->where('user_id', $u->id);
+            }
+        }
 
         // check if model is MovieModel , set status =active
         if ($modelName == 'MovieModel') {
