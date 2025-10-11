5c4e5c7d51dac43107726b55c6216ed09bcf233e
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-10-11 09:35:17 +0300

    fix: Uncomment logged-in user ID retrieval in index method for user context

--- app/Http/Controllers/DynamicCrudController.php
+++ app/Http/Controllers/DynamicCrudController.php
@@ -198,7 +198,7 @@ class DynamicCrudController extends Controller
             }
         }
         
-        // $logged_in_user_id = $request->header('logged_in_user_id');
+        $logged_in_user_id = $request->header('logged_in_user_id');
  
         // if ($logged_in_user_id && !$u) $u = User::find($logged_in_user_id);
 
