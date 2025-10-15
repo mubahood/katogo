281dafdb48ece38f92670d10ae02ca8691160791
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-10-15 16:02:41 +0300

    feat: Enhance TrendingNotification grid with sortable columns and custom display formats

--- app/Admin/Controllers/TrendingNotificationController.php
+++ app/Admin/Controllers/TrendingNotificationController.php
@@ -25,22 +25,55 @@ class TrendingNotificationController extends AdminController
     protected function grid()
     {
         $grid = new Grid(new TrendingNotification());
+        $grid->model()->orderBy('id', 'desc');
 
-        $grid->column('id', __('Id'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
-        $grid->column('movie_model_id', __('Movie model id'));
-        $grid->column('title', __('Title'));
-        $grid->column('type', __('Type'));
-        $grid->column('image_url', __('Image url'));
-        $grid->column('description', __('Description'));
-        $grid->column('views_count', __('Views count'));
-        $grid->column('views_time', __('Views time'));
-        $grid->column('url', __('Url'));
-        $grid->column('trending_time', __('Trending time'));
-        $grid->column('day_time', __('Day time'));
-        $grid->column('is_sent', __('Is sent'));
-        $grid->column('sent_time', __('Sent time'));
+        $grid->column('id', __('Id'))->sortable();
+        $grid->column('created_at', __('Created at'))->sortable()
+            ->display(function ($created_at) {
+                return date('Y-m-d H:i:s', strtotime($created_at));
+            });
+        $grid->column('movie_model_id', __('Movie model'))
+            ->display(function ($movie_model_id) {
+                $movie = \App\Models\Movie::find($movie_model_id);
+                if ($movie) {
+                    return $movie->title . " (ID: {$movie->id})";
+                } else {
+                    return "N/A";
+                }
+            })
+            ->sortable();
+        $grid->column('type', __('Type'))->hide();
+        $grid->column('image_url', __('Image url'))->lightbox(['width' => 50, 'height' => 50])->sortable();
+        $grid->column('description', __('Description'))->limit(50)->$grid->column('views_count', __('Views count'));
+        $grid->column('views_time', __('Views time'))->sortable()
+            ->display(function ($views_time) {
+                //minutes to hours
+                $hours = floor($views_time / 3600);
+                $minutes = floor(($views_time % 3600) / 60);
+                $seconds = $views_time % 60;
+                return "{$hours}h {$minutes}m {$seconds}s";
+            });
+        $grid->column('url', __('Url'))->hide();
+        $grid->column('trending_time', __('Trending time'))->sortable()
+            ->display(function ($trending_time) {
+                return date('Y-m-d H:i:s', strtotime($trending_time));
+            });
+        $grid->column('day_time', __('Day time'))->sortable();
+        $grid->column('is_sent', __('Is sent'))
+            ->label([
+                'Yes' => 'success',
+                'No' => 'default',
+            ])
+            ->sortable()
+            ->filter([
+                'Yes' => 'Yes',
+                'No' => 'No',
+            ]);
+        $grid->column('sent_time', __('Sent time'))
+            ->sortable()
+            ->display(function ($sent_time) {
+                return $sent_time ? date('Y-m-d H:i:s', strtotime($sent_time)) : 'N/A';
+            }); 
 
         return $grid;
     }
