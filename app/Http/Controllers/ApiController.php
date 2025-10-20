3a62fea76eb6f6faaa6ce93c0c8dd1ba9c2a0934
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-10-20 20:33:11 +0300

    fix: Correctly assign app type to user in manifest method

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -725,8 +725,8 @@ class ApiController extends BaseController
                 $app_type = Utils::get_app_type($r);
                 $app_types = ['ugflix', 'lugaflix'];
                 if (!in_array($app_type, $app_types)) {
+                    $u->app_type = $app_type;
                 }
-                $u->app_type = $app_types;
                 $platform = Utils::get_platform_from_request($r);
                 if ($platform != null) {
                     $u->platform = $platform;
