fecad4075f182520ece55051958dfdc884f58906
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2026-04-03 17:36:45 +0300

    Fix: remove LaravelLogViewer from app providers (dev-only package, uses auto-discovery)

--- config/app.php
+++ config/app.php
@@ -184,7 +184,6 @@ return [
         App\Providers\FirebaseServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class, 
         Berkayk\OneSignal\OneSignalServiceProvider::class,
-        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
 
     ])->toArray(),
 
